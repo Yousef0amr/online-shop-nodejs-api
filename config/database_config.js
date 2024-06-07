bf89115fcb73681719e96fe039aa7025a75f26bf
Youssief Amr Mohamed <youssief192677@fci.bu.edu.eg>
2024-06-08 02:22:58 +0300

    vercal

--- config/database_config.js
+++ config/database_config.js
@@ -1,13 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('shopdatabasepg', 'shopdatabasepg_user', '0DXnAYzqB493G3IMWmZTdbhepGff7fbt', {
-    host: 'dpg-cpgjndmct0pc739q85k0-a',
-    port: 5432,
-    dialect: 'postgres',
-    logging: false
-});
+const sequelize = new Sequelize('postgres://default:W0SiZprxEs5K@ep-morning-salad-a4tikz85.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require');
 
-console.log(process.env)
 
 export default sequelize
 
